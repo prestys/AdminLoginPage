016bc0237144ac4d49c3a756b752047b1fac81b1
Jay Prest <125089458+prestys@users.noreply.github.com>
2024-09-30 09:41:13 +0100

    checks now pulling through

--- ParagonID.Data/Models/MaintenanceDB_Daily.cs
+++ ParagonID.Data/Models/MaintenanceDB_Daily.cs
@@ -9,16 +9,16 @@ namespace ParagonID.Data.Models;
 
 public partial class MaintenanceDB_Daily : BaseEntity
 {
-    public string Employee { get; set; }
-    public string Press { get; set; }
-    public DateTime Datecompleted { get; set; }
-    public bool Check1 { get; set; }
-    public bool Check2 { get; set; }
-    public bool Check3 { get; set; }
-    public bool Check4 { get; set; }
-    public string Comment { get; set; }
+    public string? Employee { get; set; }
+    public string? Press { get; set; }
+    public DateTime? Datecompleted { get; set; }
+    public bool? Check1 { get; set; }
+    public bool? Check2 { get; set; }
+    public bool? Check3 { get; set; }
+    public bool? Check4 { get; set; }
+    public string? Comment { get; set; }
     [Column("Deleted?")]
-    public string Deleted { get; set; }
-    public bool Check5 { get; set; }
-    public bool Check6 { get; set; }
+    public string? Deleted { get; set; }
+    public bool? Check5 { get; set; }
+    public bool? Check6 { get; set; }
 }
--- ParagonID.InternalSystem/Pages/Presses/Dashboard.razor.cs
+++ ParagonID.InternalSystem/Pages/Presses/Dashboard.razor.cs
@@ -18,31 +18,69 @@ public class DashboardModel : ComponentBase
 
     protected override Task OnInitializedAsync()
     {
-        IEnumerable<MaintenanceDB_PressSpec> __presses = DataRetriever.GetAll<MaintenanceDB_PressSpec>();
-        PressFormat(__presses);
+        IEnumerable<MaintenanceDB_PressSpec> __machines = DataRetriever.GetAll<MaintenanceDB_PressSpec>();
+        MachineFormat(__machines);
 
         return base.OnInitializedAsync();
     }
 
-    private void PressFormat(IEnumerable<MaintenanceDB_PressSpec> presses)
+    private void MachineFormat(IEnumerable<MaintenanceDB_PressSpec> machines)
     {
-        string __date = DateTime.Now.ToString("d");
         string __pressRegex = @"^p\d{1,2}$";
+        DateTime __today = DateTime.Now;
+        DateTime __weekAgo = __today.AddDays(-7);
+        DateTime __monthAgo = __today.AddMonths(-1);
 
-        foreach (var __press in presses)
+        foreach (var __machine in machines)
         {
             Press __newPress = new Press();
 
-            if (Regex.Match(__press.Press, __pressRegex, RegexOptions.IgnoreCase).Success)
+            if (Regex.Match(__machine.Press, __pressRegex, RegexOptions.IgnoreCase).Success)
             {
-                IEnumerable<MaintenanceDB_Daily> __daily = DataRetriever.GetAll<MaintenanceDB_Daily>();
-                if(__daily != null)
-                {
-                    __newPress.Daily = __daily.FirstOrDefault()!.Datecompleted.ToString("d");
-                }
+                __newPress = FormatPress(__machine, __today, __weekAgo, __monthAgo);
+                __newPress.Name = __machine.Press;
             }
 
             Presses.Add(__newPress);
         }
+    }
+
+    private Press FormatPress(MaintenanceDB_PressSpec press, DateTime today, DateTime weekAgo, DateTime monthAgo)
+    {
+        Press __newPress = new Press();
+
+        IEnumerable<MaintenanceDB_Daily> __daily = DataRetriever.Get<MaintenanceDB_Daily>(
+        f => f.Datecompleted.HasValue &&
+                f.Datecompleted.Value.Date == today.Date &&
+                string.Equals(f.Press, press.Press, StringComparison.OrdinalIgnoreCase));
+
+        if (__daily.Any())
+        {
+            __newPress.Daily = __daily.First().Datecompleted?.ToString("t")!;
+        }
+
+        IEnumerable<MaintenanceDB_Daily> __weekly = DataRetriever.Get<MaintenanceDB_Daily>(
+            f => f.Datecompleted.HasValue &&
+                    f.Datecompleted.Value.Date >= weekAgo.Date &&
+                    f.Datecompleted.Value.Date <= today.Date &&
+                    string.Equals(f.Press, press.Press, StringComparison.OrdinalIgnoreCase));
+
+        if (__weekly.Any())
+        {
+            __newPress.Weekly = __weekly.First().Datecompleted?.ToString("d")!;
+        }
+
+        IEnumerable<MaintenanceDB_Daily> __monthly = DataRetriever.Get<MaintenanceDB_Daily>(
+            f => f.Datecompleted.HasValue &&
+                    f.Datecompleted.Value.Date >= monthAgo.Date &&
+                    f.Datecompleted.Value.Date <= today.Date &&
+                    string.Equals(f.Press, press.Press, StringComparison.OrdinalIgnoreCase));
+
+        if (__monthly.Any())
+        {
+            __newPress.Monthly = __monthly.First().Datecompleted?.ToString("d")!;
+        }
+
+        return __newPress;
     }
 }
